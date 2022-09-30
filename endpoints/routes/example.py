6288cd9547888e754bc60fe209f25a39d1d5bce7
MdAbdullahAlMahin <u3576752@connect.hku.hk>
2022-09-30 11:19:56 +0800

    py

--- endpoints/routes/example.py
+++ endpoints/routes/example.py
@@ -12,5 +12,7 @@ logger = logging.getLogger(__name__)
 @app.route('/example', methods=['GET'])
 def example():
     client = pymongo.MongoClient("mongodb+srv://MdAbdullahAlMahin:"+os.getenv('ATLAS_PASS')+"@cluster0.7nvntxs.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
-    db = client.testDB
-    return jsonify(output)
+    db = client['testDB']
+    collection = db['testCol']
+    doc_count = collection.count_documents({})
+    return jsonify(doc_count)
