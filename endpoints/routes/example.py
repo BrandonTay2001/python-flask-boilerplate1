00bf9ea181cdb9bb92290980a761fe870f46df5f
BrandonTay2001 <brandontay2001@gmail.com>
2022-09-30 11:39:51 +0800

    ntg

--- endpoints/routes/example.py
+++ endpoints/routes/example.py
@@ -1,7 +1,7 @@
 import logging
 import json
 from pymongo import MongoClient
-import os
+from os import environ
 
 from flask import request, jsonify
 
@@ -9,11 +9,15 @@ from endpoints import app
 
 logger = logging.getLogger(__name__)
 
+from dotenv import load_dotenv
+load_dotenv()
 
 @app.route('/example', methods=['GET'])
 def example():
-    client = MongoClient("mongodb+srv://MdAbdullahAlMahin:"+os.getenv('ATLAS_PASS')+"@cluster0.7nvntxs.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+    client = MongoClient("mongodb+srv://MdAbdullahAlMahin:MvPSCHBVtcW4w8Tx@cluster0.7nvntxs.mongodb.net/?retryWrites=true&w=majority")
     db = client['testDB']
     collection = db['testCol']
     doc_count = collection.count_documents({})
-    return jsonify(doc_count)
+    print(doc_count)
+    # return jsonify(doc_count)
+    return "hello"
